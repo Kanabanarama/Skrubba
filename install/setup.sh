8555c9f3f66584d2bc569b3159a587340dc9582d
Kana <kanabanarama@googlemail.com>
2019-01-19 02:14:36 +0100

    Server conf fixes

--- install/setup.sh
+++ install/setup.sh
@@ -19,8 +19,8 @@ sudo supervisorctl status
 
 # nginx conf
 echo "Setting up nginx"
-sudo cp /home/pi/skrubba/install/skrubba.conf /etc/nginx/sites-available/skrubba.conf
-sudo ln -fs /etc/nginx/sites-available/skrubba.nginx /etc/nginx/sites-enabled/skrubba
+sudo cp /home/pi/skrubba/install/skrubba.conf /etc/nginx/sites-available/skrubba
+sudo ln -fs /etc/nginx/sites-available/skrubba /etc/nginx/sites-enabled/skrubba
 sudo [ -e /etc/nginx/sites-enabled/default ] && rm /etc/nginx/sites-enabled/default
 
 sudo service nginx reload
